4b09c44e758d99e8f2a9a6d1af3b1725300628e9
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-07-17 14:51:23 +0900

    Title: 금 채굴하기, Time: 298ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/금 채굴하기/gold-mining.cpp
+++ 240717/금 채굴하기/gold-mining.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 int n;
-void prop(vector<vector<bool>>& visited, int cnt,int k,int x,int y){
-    visited[x][y]=true;
+void prop(vector<vector<int>>& visited, int cnt,int k,int x,int y){
+    visited[x][y]=cnt;
     if(cnt<k){
-        if(x-1>=0 && !visited[x-1][y]){
-            visited[x-1][y]=true;
+        int arr1=visited[x-1][y];
+        int arr2=visited[x][y-1];
+        int arr3=visited[x+1][y];
+        int arr4=visited[x][y+1];
+        if(x-1>=0&&(visited[x-1][y]==-1||visited[x-1][y]>cnt+1)){
             prop(visited,cnt+1,k,x-1,y);
         }
-        if(y-1>=0&& !visited[x][y-1]){
-            visited[x][y-1]=true;
+        if(y-1>=0&&(visited[x][y-1]==-1||visited[x][y-1]>cnt+1)){
             prop(visited,cnt+1,k,x,y-1);
         }
-        if(x+1<n&&!visited[x+1][y]){
-            visited[x+1][y]=true;
+        if(x+1<n&&(visited[x+1][y]==-1||visited[x+1][y]>cnt+1)){
             prop(visited,cnt+1,k,x+1,y);
         }
-        if(y+1<n&&!visited[x][y+1]){
-            visited[x][y+1]=true;
+        if(y+1<n&&(visited[x][y + 1]==-1||visited[x][y+1]>cnt+1)){
             prop(visited,cnt+1,k,x,y+1);
         }
     }
@@ -44,23 +44,21 @@ int main() {
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                vector<vector<bool>> visited(n,vector<bool>(n,false));
+                vector<vector<int>> visited(n,vector<int>(n,-1));
                 int goldcnt=0;
-                int cost=0;
                 prop(visited,0,k,i,j);
                 for(int a=0;a<n;a++){
                     for(int b=0;b<n;b++){
-                        if(visited[a][b]&&numlist[a][b]==1){
+                        if(visited[a][b]!=-1&&numlist[a][b]==1){
                             goldcnt++;
-                        }
-                        if(visited[a][b]){
-                            cost++;
                         }
                     }
                 }
                 //cout<<"K"<<k<<i<<","<<j<< "gold : "<<goldcnt<<"\n";
                 if(goldcnt*m>=k*k + (k+1)*(k+1)){
                     if(maxanswer<goldcnt){
+                        
+                    
                         maxanswer=goldcnt;
                     }
                 }

287cb2cf2f98b06ae30ed3afdd6dc788c0de3981
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-07-29 11:11:49 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240729/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+#include <queue>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> numlist(n,vector<int>(n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            int num;
+            cin>>num;
+            numlist[i][j]=num;
+        }
+    }
+
+    queue<pair<int,int>> q;
+    q.push({0,0});
+    vector<vector<int>> visited(n,vector<int>(n,false));
+    visited[0][0]=true;
+    while(!q.empty()){
+        auto cur = q.front();
+        q.pop();
+
+        auto x = cur.first;
+        auto y = cur.second;
+
+        if(x+1<n&&!visited[x+1][y]&&numlist[x+1][y]){
+            q.push({x+1,y});
+            visited[x+1][y]=true;
+        }
+        if(y+1<m&&!visited[x][y+1]&&numlist[x][y+1]){
+            q.push({x,y+1});
+            visited[x][y+1]=true;
+        }
+        if(x-1>=0&&!visited[x-1][y]&&numlist[x-1][y]){
+            q.push({x-1,y});
+            visited[x-1][y]=true;
+        }
+        if(y-1>=0&&!visited[x][y-1]&&numlist[x][y-1]){
+            q.push({x,y-1});
+            visited[x][y-1]=true;
+        }
+
+
+    }
+
+    cout<<visited[n-1][m-1];
+    return 0;
+}

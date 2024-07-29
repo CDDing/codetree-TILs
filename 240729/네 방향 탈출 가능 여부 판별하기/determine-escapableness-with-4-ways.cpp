517afefd0c879808d4acc2d5263811ea8a66610a
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-07-29 11:13:57 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 240729/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     int n,m;
     cin>>n>>m;
-    vector<vector<int>> numlist(n,vector<int>(n,0));
+    vector<vector<int>> numlist(n,vector<int>(m,0));
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             int num;

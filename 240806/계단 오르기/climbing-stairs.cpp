747ae1febf93570b067d8fd2a996662a6d2e9336
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-08-06 11:07:59 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> numlist(1001,0);
+int main() {
+    int n;
+    cin>>n;
+
+    numlist[1]=0;
+    numlist[2]=1;
+    numlist[3]=1;
+    for(int i=4;i<=n;i++){
+        numlist[i]=numlist[i-2]+numlist[i-3];
+    }
+
+    cout<<numlist[n];
+
+    return 0;
+}

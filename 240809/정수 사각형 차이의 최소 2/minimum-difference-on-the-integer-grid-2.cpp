8700cec5ae24450afe496d2892b34af547399ef9
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-08-09 21:12:53 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 240809/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -41,16 +41,25 @@ int main() {
                 auto minnum2 = min(numlist[i][j],dp[i][j-1].first);
                 auto maxnum2 = max(numlist[i][j],dp[i][j-1].second);
 
-                if(abs(minnum-maxnum)<abs(minnum2-maxnum2)){
-                    dp[i][j] = {minnum,maxnum};
+                if(abs(minnum-maxnum)>abs(minnum2-maxnum2)){
+                    
+                    dp[i][j] = {minnum2,maxnum2};
+                    
                 }
                 else{
-                    dp[i][j] = {minnum2,maxnum2};
+                    dp[i][j] = {minnum,maxnum};
                 }
                 
                 // dp[i][j] = numlist[i][j] +max(dp[i-1][j],dp[i][j-1]);
             }
         }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            // cout<<"("<<dp[i][j].first<<","<<dp[i][j].second<<")"<<" ";
+        }
+        // cout<<endl;
     }
 
     cout<<abs(dp[n-1][n-1].first - dp[n-1][n-1].second);

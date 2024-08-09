071ce2edef86e5ae465819ece3026edf5a305c48
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-08-09 20:58:34 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<vector<int>> numlist(n,vector<int>(n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            
+            int num;
+            cin>>num;
+            numlist[i][j]=num;
+        }
+    }
+    vector<vector<pair<int,int>>> dp(n,vector<pair<int,int>>(n,{0,0}));
+    dp[0][0]={numlist[0][0],numlist[0][0]};
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0&&j==0){
+                
+            }
+            else if(i==0){
+                auto minnum = min(numlist[i][j],dp[i][j-1].first);
+                auto maxnum = max(numlist[i][j],dp[i][j-1].second);
+                dp[i][j] ={minnum,maxnum};
+            }
+            else if(j==0){
+                auto minnum = min(numlist[i][j],dp[i-1][j].first);
+                auto maxnum = max(numlist[i][j],dp[i-1][j].second);
+                dp[i][j] = {minnum,maxnum};
+            }
+            else{
+                auto minnum = min(numlist[i][j],dp[i-1][j].first);
+                auto maxnum = max(numlist[i][j],dp[i-1][j].second);
+
+                auto minnum2 = min(numlist[i][j],dp[i][j-1].first);
+                auto maxnum2 = max(numlist[i][j],dp[i][j-1].second);
+
+                if(abs(minnum-maxnum)<abs(minnum2-maxnum2)){
+                    dp[i][j] = {minnum,maxnum};
+                }
+                else{
+                    dp[i][j] = {minnum2,maxnum2};
+                }
+                
+                // dp[i][j] = numlist[i][j] +max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+
+    cout<<abs(dp[n-1][n-1].first - dp[n-1][n-1].second);
+    return 0;
+}

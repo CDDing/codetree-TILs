8334cb1f9e70be7873a7994dfbf2340819e107be
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-08-11 13:48:37 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<vector<int>> numlist(n,vector<int>(n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            
+            int num;
+            cin>>num;
+            numlist[i][j]=num;
+        }
+    }
+    vector<vector<int>> dp(n,vector<int>(n,0));
+    dp[0][0]=numlist[0][0];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0&&j==0){
+                
+            }
+            else if(i==0){
+                dp[i][j] =max(numlist[i][j], dp[i][j-1]);
+            }
+            else if(j==0){
+                dp[i][j] = max(numlist[i][j],dp[i-1][j]);
+            }
+            else{
+                dp[i][j] = max(numlist[i][j],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+    }
+
+    cout<<dp[n-1][n-1];
+    return 0;
+}
